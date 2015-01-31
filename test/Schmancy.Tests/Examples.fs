04e2f96d43503f02e04763b8001c53d6dcbf4bc2
Amir Barylko <amir@barylko.com>
2015-01-31 00:37:43 -0600

    Added implementation multiple requests to different resources

--- test/Schmancy.Tests/Examples.fs
+++ test/Schmancy.Tests/Examples.fs
@@ -31,20 +31,48 @@ module ``Stubbing for any method`` =
 
 module ``Returning JSON`` =
 
-    type Customer = {Name:string;Age:int}
+    open Newtonsoft.Json
+
+    type Customer = {Name:string; Id:int}
 
     [<Test>]
     let ``When the call returns JSON`` () =
         let request = new RestRequest("/customers", Method.GET)
         request.RequestFormat <- DataFormat.Json
 
+        let customer = {Name="Charles Magnus"; Id=3}
+
         stubRequest url RequestType.Get "/customers"
-        |> withJsonResponse "{customer:'Charles Magnus'}"
+        |> withJsonResponse  (JsonConvert.SerializeObject customer)
         |> hostAndCall (fun _ -> 
             let response = client.Execute(request)
-            response.StatusCode, response.Content
+            response.StatusCode, JsonConvert.DeserializeObject<Customer>(response.Content)
           )
-        |> should equal (System.Net.HttpStatusCode.OK, "{customer:'Charles Magnus'}")
+        |> should equal (System.Net.HttpStatusCode.OK, customer)
+
+
+    [<Test>]
+    let ``When calling multiple paths`` () =
+        let getRequest (path:string) =
+            let request = new RestRequest(path, Method.GET)
+            request.RequestFormat <- DataFormat.Json
+            client.Execute(request)
+
+        stubRequest url RequestType.Get "/customers"
+        |> withJsonResponse "{customers:[1, 2, 3, 4, 5, 6]}"
+        |> andStub RequestType.Get "/customers/1"
+        |> withJsonResponse "{name: 'Charles', id: 1}"
+        |> hostAndCall (fun _ -> 
+            let r1 = getRequest("/customers")
+            let r2 = getRequest("/customers/1")
+
+            r1.StatusCode, r2.StatusCode, r1.Content, r2.Content
+          )
+        |> should equal (System.Net.HttpStatusCode.OK, System.Net.HttpStatusCode.OK,
+                         "{customers:[1, 2, 3, 4, 5, 6]}", 
+                         "{name: 'Charles', id: 1}"
+                         )
+
 
 module ``Stubbing with body and headers`` =
 
